b63d4ff7eacc874652bb80c28187fd16bcb384d8
Max <Max>
2016-06-08 17:29:45 -0300

    Benito Matrix

---
+++ benito_matrix/main.cpp
@@ -0,0 +1,70 @@
+/*
+ * LeisMunicipais.com.br - Liz Serviços Online
+ * 
+ * O conteúdo deste arquivo está protegido pela Lei de Direitos Autorais.
+ * É vetada a reprodução e/ou utilização de parte ou completo conteúdo deste arquivo.
+ * 
+ *  Versão: 
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Max
+ *
+ * Created on 8 de Junho de 2016, 16:11
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+void matrixPopulate(int ** matrix, int rows, int cols);
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    int ** matrix;
+    int n;
+    
+    // Ask user
+    cout << "Por Favor, informe a quantidade de probabilidades:" << endl;
+    cin >> n;
+    
+    if (n){
+        // Creating rows
+        matrix = new int*[(int)pow(2,n)];
+        
+        for (int x; x<pow(2,n); x++){
+            matrix[x] = new int[n];
+        }
+        
+        // Populate
+        matrixPopulate(matrix, (int)pow(2, n), n);
+        
+    } else {
+        cout << "Erro: o valor precisa ser inteiro" << endl;
+    }
+    
+    return 0;
+}
+
+void matrixPopulate(int ** matrix, int rows, int cols){
+    for (int y=0; y<rows; y++){
+        
+        for(int x=0; x<cols; x++){
+            if (y & (int)pow(2,x)){
+                cout <<  1;
+                cout << " ";
+            } else {
+                cout <<  0;
+                cout << " ";
+            }
+        }
+        cout << endl;
+        
+    }
+}
